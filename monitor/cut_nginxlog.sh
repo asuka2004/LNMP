c59e95162850ff8fbe61fb22dec39e0e021d7418
Kung <asuka.2004@gmail.com>
2022-08-26 22:20:18 +0800

    modify file

--- monitor/cut_nginxlog.sh
+++ monitor/cut_nginxlog.sh
@@ -6,13 +6,8 @@
 # System      : CentOS 7.9 
 			       
 export PS4='++ ${LINENO}'  
-export LANG=C
 export PATH=$PATH
 [ -f /etc/init.d/functions ] && . /etc/init.d/functions
-Script_Path=/root/project/github
-[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
-Log_Path=/root/tmp
-[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 DataFormat=`date +%Y%m%d -d -1day`
 Basedir="/app/nginx"
--- monitor/resist_ddos.sh
+++ monitor/resist_ddos.sh
@@ -6,13 +6,8 @@
 # System      : CentOS 7.9 
 			       
 export PS4='++ ${LINENO}'  
-export LANG=C
 export PATH=$PATH
 [ -f /etc/init.d/functions ] && . /etc/init.d/functions
-Script_Path=/root/project/github
-[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
-Log_Path=/root/tmp
-[ ! -d ${Log_Path} ] && mkdir -p ${Log_Path}
 
 file=$1
 while true
