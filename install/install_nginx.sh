7a38b48d005958b71692dbe47cad737e0115bfc8
Kung <asuka.2004@gmail.com>
2022-08-30 21:45:38 +0800

    modify install script

--- install/install_nginx.sh
+++ install/install_nginx.sh
@@ -13,6 +13,8 @@ App_Path=/app
 [ ! -d ${App_Path} ] && mkdir -p ${App_Path}
 Soft_Path=/tool/software
 [ ! -d ${Soft_Path} ] && mkdir -p ${Soft_Path}
+Script_Path=/tool/script
+[ ! -d ${Script_Path} ] && mkdir -p ${Script_Path}
 
 Check_User(){
 	if [ $UID -ne 0 ]
@@ -22,34 +24,71 @@ Check_User(){
 	fi	
 }
 
+Setup_User(){
+        echo "Add User Nginx. Please wait.................."
+	useradd -u 1111 -g nginx -s /sbin/nologin -M nginx
+
+}
+
 Update_Depend(){
+	echo "Install Depend software. Please wait........."
 	yum -y install gcc zlib zlib-devel pcre pcre-devel openssl openssl-devel
+	if [ $? -eq 0 ] 
+         then
+                 action "Success to install Depend " /bin/true
+        else
+                 action "Fail to install Depend" /bin/false
+                 exit
+        fi  
 }
 
 Install_Nginx(){
+        echo "Install Nginx. Please wait.................."
 	cd ${Soft_Path}	
-	wget https://nginx.org/download/nginx-1.18.0.tar.gz 
-	useradd nginx -u 1111 -s /sbin/nologin -M
+	#wget https://nginx.org/download/nginx-1.18.0.tar.gz 
 	tar -zxvf  nginx-1.18.0.tar.gz 
 	cd nginx-1.18.0/
 	./configure --user=nginx --group=nginx --prefix=/app/nginx-1.18.0 --with-http_stub_status_module --with-http_ssl_module
 	make;make install
-	ln -s ${App_Path}/nginx-1.18.0  ${App_Path}/nginx
-	echo 'export PATH="${App_Path}/nginx/sbin:$PATH"'>>/etc/profile
+	if [ $? -eq 0 ] 
+         then
+                 action "Success to install Nginx " /bin/true
+        else
+                 action "Fail to install Nginx" /bin/false
+                 exit
+        fi  
 
+	ln -s ${App_Path}/nginx-1.18.0  ${App_Path}/nginx
+	echo 'export PATH=${App_Path}/nginx/sbin:$PATH'>>/etc/profile
+	source /etc/profile
 }
 
 Setup_daemon(){
-	cp ${Soft_Path}/nginx.service /etc/systemd/system/nginx.service
+        echo "Setup daemon. Please wait...................."
+	cp ${Script_Path}/nginx.service /etc/systemd/system/nginx.service
 	systemctl daemon-reload
   	systemctl enable mysqld
 	systemctl start mysqld
+	if [ $? -eq 0 ] 
+         then
+                 action "Success to daemon " /bin/true
+        else
+                 action "Fail to daemon" /bin/false
+                 exit
+        fi  
+
+
 }
 
 main(){
 	Check_User
-	Update_Depend	
+	echo -e "------------------------------------------------------------------- \n"   
+	Setup_User
+	echo -e "------------------------------------------------------------------- \n"
+	Update_Depend
+	echo -e "------------------------------------------------------------------- \n"   	
 	Install_Nginx
+	echo -e "------------------------------------------------------------------- \n"   
 	Setup_daemon
 }
 main 
